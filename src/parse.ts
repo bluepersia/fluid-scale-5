c864a9b9295dfb148cc21c729621dc7ed6aec48c
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-09-13 17:10:35 -0600

    Batch stylesheet

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,16 @@
-import { DocumentClone, MediaRuleClone, StyleSheetClone } from "./cloner.types";
-import { CSSParseResult } from "./parse.types";
+import {
+  CSSRuleClone,
+  DocumentClone,
+  MediaRuleClone,
+  StyleRuleClone,
+  StyleSheetClone,
+} from "./cloner.types";
+import {
+  BatchRuleResult,
+  CSSParseResult,
+  StyleBatch,
+  StyleBatchState,
+} from "./parse.types";
 
 const STYLE_RULE_TYPE = 1;
 const MEDIA_RULE_TYPE = 4;
@@ -37,4 +48,71 @@ function parseStyleSheet(
   ) as MediaRuleClone;
 
   const baselineWidth = baselineMediaRule?.minWidth ?? globalBaselineWidth;
+}
+
+function batchStyleSheet(
+  styleSheet: StyleSheetClone,
+  baselineWidth: number
+): StyleBatch[] {
+  let styleBatchState: StyleBatchState = {
+    currentStyleBatch: null,
+    styleBatches: [],
+  };
+
+  for (const rule of styleSheet.cssRules) {
+    styleBatchState = {
+      ...styleBatchState,
+      ...batchRule(rule, styleBatchState, baselineWidth),
+    };
+  }
+
+  return styleBatchState.styleBatches;
+}
+
+function batchRule(
+  rule: CSSRuleClone,
+  styleBatchState: StyleBatchState,
+  baselineWidth: number
+): StyleBatchState {
+  const newStyleBatchState = { ...styleBatchState };
+
+  if (rule.type === STYLE_RULE_TYPE) {
+    const styleRule = rule as StyleRuleClone;
+    newStyleBatchState.currentStyleBatch = handleNewStyleRuleBatch(
+      newStyleBatchState.currentStyleBatch,
+      baselineWidth
+    );
+    newStyleBatchState.currentStyleBatch = {
+      ...newStyleBatchState.currentStyleBatch!,
+      rules: [...newStyleBatchState.currentStyleBatch!.rules, styleRule],
+    };
+  } else if (rule.type === MEDIA_RULE_TYPE) {
+    newStyleBatchState.currentStyleBatch = null;
+    newStyleBatchState.styleBatches.push(
+      newMediaRuleBatch(rule as MediaRuleClone)
+    );
+  }
+
+  return newStyleBatchState;
+}
+
+function handleNewStyleRuleBatch(
+  currentStyleBatch: StyleBatch | null,
+  baselineWidth: number
+): StyleBatch | null {
+  return (
+    currentStyleBatch || {
+      width: baselineWidth,
+      rules: [],
+      isMediaRule: false,
+    }
+  );
+}
+
+function newMediaRuleBatch(mediaRule: MediaRuleClone): StyleBatch {
+  return {
+    width: mediaRule.minWidth,
+    rules: mediaRule.cssRules,
+    isMediaRule: true,
+  };
 }
